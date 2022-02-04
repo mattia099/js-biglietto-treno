72efc108645ff5965c83f85b0549f701d81bd114
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-04 14:12:45 +0100

    Aggiunti controlli

--- jscript.js
+++ jscript.js
@@ -2,13 +2,20 @@ const kmPercorsi = prompt('Quanti kilometri vuoi percorrere?');
 const etàPassegero = prompt('Quanti anni hai?');
 var prezzoBiglietto = kmPercorsi * 0.21;
 
-
-if(etàPassegero < 18){
-    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20) / 100;
+if(isNaN(kmPercorsi) || isNaN(etàPassegero) ){
+    alert('Scrivi numeri')
+}else if(kmPercorsi < 0 || etàPassegero <0  ){
+    alert('Numeri negativi non validi')
 }
-else if(etàPassegero >65){
-    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40) / 100;;
-}
+else{
 
-alert(prezzoBiglietto.toFixed(2) + "€");
-console.log(prezzoBiglietto)
+    if(etàPassegero < 18){
+        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20) / 100;
+    }
+    else if(etàPassegero >65){
+        prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 40) / 100;;
+    }
+
+    alert(prezzoBiglietto.toFixed(2) + "€");
+    console.log(prezzoBiglietto)
+}
